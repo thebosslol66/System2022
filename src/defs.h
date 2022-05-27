3e60057448db6d95985d560a07380b4a3f41773f
nachos <lucienmartial33@gmail.com>
2022-05-27 16:18:25 +0200

    finalCommit

--- src/defs.h
+++ src/defs.h
@@ -1,5 +1,7 @@
 #define _DEFAULT_SOURCE
 
+#include "message.h"
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,31 +14,37 @@
 #include <stdbool.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
-#include "message.h"
+
+#define PATH_PID_FILE_SERVER "/tmp/game_server.pid"
+#define PATH_FIFO_GAME_SERVER "/tmp/game_server.fifo"
+#define PATH_DIR_GAME_SERVER "/tmp/game_server"
+
+#define CLI_EXT "_cli"
+#define LENGTH_OF_CLI_EXT 4
+#define PID_BUFFER 16
 
 /////////////////////////////////////////////////////////////////////////////
 
 #define PID_AD_FILE "/tmp/game_server.pid"
 #define SERVER_FIFO "/tmp/game_server.fifo"
 #define FIFO_DIR "/tmp/game_server"
-#define FIFO_MODE 0755
 
 /////////////////////////////////////////////////////////////////////////////
-
-#define PATH_PID_FILE_SERVER "/tmp/game_server.pid"
-#define PATH_FIFO_GAME_SERVER "/tmp/game_server.fifo"
-#define PATH_DIR_GAME_SERVER "/tmp/game_server"
 
 #define LENGTH_OF_PATH_DIR 16
 #define LENGTH_OF_CLIENT_FIFOS 64
 #define LENGTH_OF_PID_BUFFER 16
 #define SERV_EXT "_serv"
 #define LENGTH_OF_SERV_EXT 5
+#define FIFO_MODE 0755
 
 #define SERV_PID_MODE 0664
 #define DIR_MODE 0755
 #define SERVER_PID_SIZE 16
+
+// SERVER DEFS
 
 void handSIGUSR1(int sig);
 void handENDSRV(int sig);
@@ -54,6 +62,8 @@ bool game_exists(char *game);
 bool edit_table(void);
 pid_t start_game(void);
 void interpret_end_of_game(const int childEnding, const pid_t childPID);
+
+// SERVER DEFS
 
 #ifdef DEBUG
 #define dperror(a) perror(a)
